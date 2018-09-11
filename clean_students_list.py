f521beb5014d178499c255b53ea91fa7f937bf00
Benjamin Huang <bhuang@rms.com>
2018-09-11 14:19:32 -0700

    work version 1

--- clean_students_list.py
+++ clean_students_list.py
@@ -8,52 +8,75 @@ NEW_FILE_NAME = 'students_new.xlsx'
 ALL_STUDENTS_SHEET = '總名單'
 NEW_ALL_STUDENTS_SHEET = 'all_students'
 
+STUDENT_EMAIL_COL_IN_ALL_STUDENTS_SHEET = 4
+
 DISMISSED_STUDENTS_SHEETS = [
-  '2015新生開除名單',
-  '2016開除名單',
-  '2018 開除名單'
+    '2018 開除名單',
+    '2016開除名單',
+    '2015新生開除名單'
 ]
 
-STUDENT_NUM_COL_IN_DISMISSED_SHEETS = [
-  1,
-  1,
-  4
+STUDENT_EMAIL_COL_IN_DISMISSED_SHEETS = [
+    6,
+    2,
+    2
 ]
 
-def isDismissed(studentNum, wb):
-  for sheet_name in DISMISSED_STUDENTS_SHEETS:
+
+def isDismissed(studentEmail, wb):
+  for index in range(len(DISMISSED_STUDENTS_SHEETS)):
+    sheet_name = DISMISSED_STUDENTS_SHEETS[index]
     sheet_dismissed = wb[sheet_name]
     row_count = sheet_dismissed.max_row
     for row in range(2, row_count + 1):
-      if sheet_dismissed.cell(row=row, column=4).value == studentNum:
-        print('Student {} is dismissed in {}'.format(studentNum, sheet_name))
+      if sheet_dismissed.cell(row=row, column=STUDENT_EMAIL_COL_IN_DISMISSED_SHEETS[index]).value == studentEmail:
+        print('Student {} is dismissed in {}'.format(studentEmail, sheet_name))
         return True
 
   return False
 
 
-if __name__ == "__main__":
-  wb = openpyxl.load_workbook(FILE_NAME)
-  sheet_all = wb[ALL_STUDENTS_SHEET]
-  
-  # Print statistics information
-  row_count_all = sheet_all.max_row
-  all_students_count = row_count_all - 1
-  print ("There are {} students in {}".format(all_students_count, ALL_STUDENTS_SHEET)
+def getRowCount(sheet, col):
+  rowCount = 0
+  max_row = sheet.max_row + 1
 
-  
+  for row in range(1, max_row):
+    if sheet.cell(row=row, column=col).value is None and sheet.cell(row=row, column=col-1).value is None:
+      break
+    else:
+      rowCount = rowCount + 1
+  return rowCount
+
+
+def statistics(wb):
+  sheet_all = wb[ALL_STUDENTS_SHEET]
+  # Print statistics information
+  row_count_all = getRowCount(
+      sheet_all, STUDENT_EMAIL_COL_IN_ALL_STUDENTS_SHEET)
+  all_students_count = row_count_all - 1
+  print("There are {} students in {}".format(
+      all_students_count, ALL_STUDENTS_SHEET))
+
   dismissed_students_count = 0
-  for sheet_name in DISMISSED_STUDENTS_SHEETS:
+  for index in range(len(DISMISSED_STUDENTS_SHEETS)):
+    sheet_name = DISMISSED_STUDENTS_SHEETS[index]
     sheet_dismissed = wb[sheet_name]
-    row_count_all = sheet_dismissed.max_row
-    students_count = row_count_all - 1
+    row_count = getRowCount(
+        sheet_dismissed, STUDENT_EMAIL_COL_IN_DISMISSED_SHEETS[index])
+    students_count = row_count - 1
     dismissed_students_count += students_count
 
-    print ("There are {} students in {}".format(students_count, sheet_name)
+    print("There are {} students in {}".format(students_count, sheet_name))
 
-  print ("There are {} students were dismissed".format(dismissed_students_count)
-  
-  sheet_result = wb.create_sheet(title = NEW_ALL_STUDENTS_SHEET)
+  print("There are {} students were dismissed".format(dismissed_students_count))
+
+
+if __name__ == "__main__":
+  wb = openpyxl.load_workbook(FILE_NAME)
+  statistics(wb)
+
+  sheet_all = wb[ALL_STUDENTS_SHEET]
+  sheet_result = wb.create_sheet(title=NEW_ALL_STUDENTS_SHEET)
 
   # copy the field names in the first row
   column_count_all = sheet_all.max_column
@@ -63,12 +86,13 @@ if __name__ == "__main__":
 
   row_result = 2
   # filter the students infor
-  for row in range(2, row_count_all + 1):
-    if isDismissed(sheet_all.cell(row=row, column=4).value, wb):
+  for row in range(2, sheet_all.max_row + 1):
+    userEmail = sheet_all.cell(row=row, column=STUDENT_EMAIL_COL_IN_ALL_STUDENTS_SHEET).value
+    if userEmail and isDismissed(userEmail, wb):
       continue
     for col in range(1, column_count_all + 1):
       sheet_result.cell(row=row_result, column=col,
                         value=sheet_all.cell(row=row, column=col).value)
     row_result += 1
 
-  wb.save('NEW_FILE_NAME')
+  wb.save(NEW_FILE_NAME)

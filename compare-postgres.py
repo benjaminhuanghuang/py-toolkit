686b3bb93c43e1b2ffddbc6140af1173c26bacd0
Benjamin Huang <bhuang@rms.com>
2019-04-01 18:38:31 -0700

    udpate

--- compare-postgres.py
+++ compare-postgres.py
@@ -5,16 +5,14 @@ import sys
 import os
 import psycopg2   # pip install psycopg2-binary
 
-table_excluded = ['session', 'sesison_location']
-column_excluded = ['created_by', 'created_at', 'modified_by', 'modified_at']
+Table_Excluded = ['session', 'session_location', 'flyway_schema_history']
+Column_Excluded = ['created_by', 'created_at', 'modified_by', 'modified_at']
 
 
 def main():
-	# Walking specified resources and initializing target with statements.
+	# 
   conn_local = psycopg2.connect(host="localhost", dbname="quantumdb", user="quantumdb", password="qu%ntu3DB")
-
-  conn_dev = psycopg2.connect(host="localhost",  port="5433", dbname="quantumdb",
-                            user="quantum_api_temp", password="akjankjdaHHAS")
+  conn_dev = psycopg2.connect(host="localhost",  port="5433", dbname="quantumdb", user="quantum_api_temp", password="akjankjdaHHAS")
 
   cursor_local = conn_local.cursor()
   cursor_dev = conn_dev.cursor()
@@ -31,13 +29,16 @@ def main():
 
   for table in tables_local:
     table_name = table[0]
-    if(not table in table_excluded):
-      compareTableData(cursor_local, cursor_dev, table[0])
+    if table_name in Table_Excluded:
+      continue
+    compareTableData(cursor_local, cursor_dev, table_name)
     
   conn_local.close()
   conn_dev.close()
 
 def compareTableData(cursor_local, cursor_dev, table_name):
+  print("#### Comparing Table {}".format(table_name))
+  
   cursor_local.execute("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_NAME = '" + table_name+"'")
   columns_local = cursor_local.fetchall()
   cursor_dev.execute("SELECT COLUMN_NAME FROM information_schema.COLUMNS WHERE TABLE_NAME = '" + table_name+"'")
@@ -65,7 +66,7 @@ def compareTableData(cursor_local, cursor_dev, table_name):
   for i in range(len(rows_local)):
     for j in range(len(columns_local)):
       column_name = columns_local[j][0]
-      if(column_name in column_excluded):
+      if(column_name in Column_Excluded):
         continue
       if(rows_local[i][j] != rows_dev[i][j]):
         print(">>>>>>>> Table {} row{} column {} in cocal is {} .".format(table_name, i,  column_name, rows_local[i][j]))
